511c043cb0ced23267ee63f96f0d1079362374a7
Jordan Roberts <jordanroberts333@icloud.com>
2025-01-01 18:46:09 +0000

    feat: added update script using yalc to root package.json

--- packages/viteFontPlugin/src/index.ts
+++ packages/viteFontPlugin/src/index.ts
@@ -4,7 +4,7 @@ export default function fontPlugin(): Plugin {
   return {
     name: "vite-font-plugin",
     config() {
-      console.log("hello world");
+      console.log("ahhh");
     },
   };
 }
