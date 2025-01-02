be51d3eeebdc604a0e614e0caaad7a842f77c308
Jordan Roberts <jordanroberts333@icloud.com>
2025-01-02 15:00:02 +0000

    feat: modified typeGen script to allow for fallback options

--- playground/react/vite.config.ts
+++ playground/react/vite.config.ts
@@ -4,5 +4,14 @@ import fontPlugin from "@strawr/vite-font-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), fontPlugin([])],
+  plugins: [
+    react(),
+    fontPlugin([
+      {
+        font: "ADLaM Display",
+        modifiedFallback: false,
+        customFallback: "",
+      },
+    ]),
+  ],
 });
