4df7d30a5ee41fde34d76b1e71487e28e140e42a
Jordan Roberts <jordanroberts333@icloud.com>
2025-01-04 17:49:12 +0000

    fix: attemptFetch function handles fetch and parse errors seperately

--- utils/attemptFetch.ts
+++ utils/attemptFetch.ts
@@ -11,14 +11,14 @@ type AttemptFetchReturnType<T extends AttemptFetchOptions> =
   T["parse"] extends "JSON"
     ? JSON
     : T["parse"] extends "text"
-      ? string
-      : T["parse"] extends "blob"
-        ? Blob
-        : T["parse"] extends "arrayBuffer"
-          ? ArrayBuffer
-          : T["parse"] extends "formData"
-            ? FormData
-            : Response;
+    ? string
+    : T["parse"] extends "blob"
+    ? Blob
+    : T["parse"] extends "arrayBuffer"
+    ? ArrayBuffer
+    : T["parse"] extends "formData"
+    ? FormData
+    : Response;
 
 export default async function attemptFetch<T extends AttemptFetchOptions>({
   url,
@@ -29,51 +29,54 @@ export default async function attemptFetch<T extends AttemptFetchOptions>({
   errorPrefix,
 }: T): Promise<AttemptFetchReturnType<T>> {
   let attempt = 0;
+  let response: Response | null = null;
+
+  // Attempt to fetch url
   while (attempt < retries) {
     try {
-      const response = await fetch(url, options);
-
+      response = await fetch(url, options);
       if (!response.ok) {
         throw new Error(
-          `Failed to fetch, status: ${response.status} ${response.statusText}`,
+          `Failed to fetch, status: ${response.status} ${response.statusText}`
         );
       }
-
-      if (parse) {
-        try {
-          switch (parse) {
-            case "JSON":
-              return (await response.json()) as AttemptFetchReturnType<T>;
-            case "text":
-              return (await response.text()) as AttemptFetchReturnType<T>;
-            case "blob":
-              return (await response.blob()) as AttemptFetchReturnType<T>;
-            case "arrayBuffer":
-              return (await response.arrayBuffer()) as AttemptFetchReturnType<T>;
-            case "formData":
-              return (await response.formData()) as AttemptFetchReturnType<T>;
-          }
-        } catch (parseError) {
-          throw new Error(
-            `${
-              errorPrefix ? `${errorPrefix} ` : ""
-            }Failed to parse '${url}' to ${parse}, Error: ${parseError}`,
-          );
-        }
-      }
-
-      return response as AttemptFetchReturnType<T>;
+      break;
     } catch (fetchError) {
       attempt++;
       if (attempt >= retries) {
         throw new Error(
           `${
             errorPrefix ? `${errorPrefix} ` : ""
-          }Failed to fetch '${url}' after ${retries} attempts: ${fetchError}`,
+          }Failed to fetch '${url}' after ${retries} attempts: ${fetchError}`
         );
       }
-      // Wait before retrying
       await new Promise((resolve) => setTimeout(resolve, delay));
     }
+  }
+
+  // If parse option provided attempt parse
+  if (response && parse) {
+    try {
+      switch (parse) {
+        case "JSON":
+          return (await response.json()) as AttemptFetchReturnType<T>;
+        case "text":
+          return (await response.text()) as AttemptFetchReturnType<T>;
+        case "blob":
+          return (await response.blob()) as AttemptFetchReturnType<T>;
+        case "arrayBuffer":
+          return (await response.arrayBuffer()) as AttemptFetchReturnType<T>;
+        case "formData":
+          return (await response.formData()) as AttemptFetchReturnType<T>;
+      }
+    } catch (parseError) {
+      throw new Error(
+        `${
+          errorPrefix ? `${errorPrefix} ` : ""
+        }Failed to parse '${url}' to ${parse}, Error: ${parseError}`
+      );
+    }
+  } else {
+    return response as AttemptFetchReturnType<T>;
   }
 }
