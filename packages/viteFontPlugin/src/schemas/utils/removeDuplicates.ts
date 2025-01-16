e618c1d0576dfcfb3cd76f7e49fb4b72699473b6
Jordan Roberts <jordanroberts333@icloud.com>
2025-01-16 20:24:37 +0000

    feat: added removeDuplicates helper function for schema

---
+++ packages/viteFontPlugin/src/schemas/utils/removeDuplicates.ts
@@ -0,0 +1,21 @@
+import Logger from "../../utils/logger.js";
+
+/**
+ * Zod helper that will remove duplicates from an array and log a warning.
+ * @param arr - The input array
+ * @param context - A string indicating which field failed (for logging purposes)
+ * @returns A unique array
+ */
+
+export default function removeDuplicates<T>(arr: T[], context: string): T[] {
+  const uniqueValues = Array.from(new Set(arr));
+  if (uniqueValues.length < arr.length) {
+    const duplicates = arr.filter(
+      (value, index) => arr.indexOf(value) !== index
+    );
+    Logger.warn(
+      `Duplicates found in ${context}: ${Array.from(new Set(duplicates))}`
+    );
+  }
+  return uniqueValues;
+}
